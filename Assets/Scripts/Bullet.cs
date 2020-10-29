bbcb0b3ff010ce80e7b6ad3dbdbcf433ad276bbc
Leandro Biloni <leandrobiloni@hotmail.com>
2020-10-29 20:36:16 -0300

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    private Vector3 _dir;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(target);
+    }
+
+    void Move(Transform target)
+    {
+        _dir = (target.position - transform.position).normalized;
+        transform.position += _dir * speed * Time.deltaTime;
+    }
+}
